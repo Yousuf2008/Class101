211e366303511a467dab19c858e3978ffeb5f713
Yousuf2008 <83379799+Yousuf2008@users.noreply.github.com>
2022-12-07 21:40:05 -0500

    Add files via upload

---
+++ roll-a-dice.py
@@ -0,0 +1,41 @@
+import random
+
+response ='y'
+
+while response=='y':
+    no = random.randint(1,6)
+
+    if no == 1:
+        print('[.      ]')
+        print('[.  0   ]')
+        print('[.      ]')
+
+    if no == 2:
+         print('[.0    ]')
+         print('[.     ]')
+         print('[.    0]')
+
+    if no == 3:
+         print('[.0    ]')
+         print('[.  0  ]')
+         print('[.    0]')
+
+    if no == 4:
+         print('[.0   0]')
+         print('[.     ]')
+         print('[.0   0]')
+
+    if no == 5:
+         print('[.0   0]')
+         print('[.  0  ]')
+         print('[.0   0]')
+
+    if no == 6:
+         print('[.0   0]')
+         print('[.0   0]')
+         print('[.0   0]')
+
+    response = print('press y to roll the dice again')
+    print('\n')
+
+           
